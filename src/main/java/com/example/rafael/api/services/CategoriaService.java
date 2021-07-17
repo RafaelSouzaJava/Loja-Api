169bba23fffb24101bce7734d954441540790e51
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-07-17 12:09:54 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/example/rafael/api/services/CategoriaService.java
+++ src/main/java/com/example/rafael/api/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.rafael.api.domain.Categoria;
 import com.example.rafael.api.repositories.CategoriaRepository;
+import com.example.rafael.api.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrato! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }

50a563a486687582159220758bdd52c139e260b2
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-07-17 11:42:10 -0300

    Criando a Class e Repository de Produto e associacao muitos para muitos

--- src/main/java/com/example/rafael/api/LojaApiApplication.java
+++ src/main/java/com/example/rafael/api/LojaApiApplication.java
@@ -1,13 +1,55 @@
 package com.example.rafael.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.rafael.api.domain.Categoria;
+import com.example.rafael.api.domain.Produto;
+import com.example.rafael.api.repositories.CategoriaRepository;
+import com.example.rafael.api.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class LojaApiApplication {
+public class LojaApiApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(LojaApiApplication.class, args);
+		SpringApplication.run(LojaApiApplication.class, args);		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Geeks");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		Produto p4 = new Produto(null, "Quadros 3D Homem de Ferro", 50.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat3.getProdutos().addAll(Arrays.asList(p4));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p4.getCategorias().addAll(Arrays.asList(cat3));
+		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
+		
 	}
 
 }
